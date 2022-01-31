ad5d2fd7e199f7661020501aa45c5608471646a6
H4mid2019 <hamidreza.darabi@hotmail.com>
2022-01-31 05:02:04 -0800

    some fixes,cat

--- cat/cat.py
+++ cat/cat.py
@@ -4,7 +4,7 @@ import random
 import os
 import redis
 import boto3
-from multiprocessing.pool import ThreadPool
+import threading
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -12,7 +12,7 @@ redis_host = os.getenv("REDIS_ADDRESS")
 redis_port = os.getenv("REDIS_PORT")
 DYNAMO_DB_TABLE = os.getenv("DYNAMO_DB_TABLE")
 redis_cache = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
-
+FACT_COUNTS = 3
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(DYNAMO_DB_TABLE)
 
@@ -29,23 +29,28 @@ def put_fact(fact, hash_primary):
 
 def read_fact(hash_primary):
     tr = table.get_item(Key={'CatFactHashKey': hash_primary})
-    return tr['Item']
+    return tr.get('Item').get("fact")
 
 
-def fetcher(url):
-    response = requests.get(url, timeout=3)
-    if response.status_code == 200:
-        return response.json().get("fact")
-
-
-urls = ["https://catfact.ninja/fact"] * 5
-pool = ThreadPool(5)
 results = []
-for i in urls:
-    results.append(pool.apply_async(fetcher, args=(i,)))
-pool.close()
-pool.join()
-results = [r.get() for r in results]
+
+
+def fetcher():
+    response = requests.get("https://catfact.ninja/fact", timeout=3)
+    if response.status_code == 200:
+        results.append(response.json().get("fact"))
+        return
+
+
+ths = []
+for _ in range(0, FACT_COUNTS+1):
+    ths.append(threading.Thread(target=fetcher))
+
+for i in ths:
+    i.start()
+del i
+for i in ths:
+    i.join()
 
 for index, fact in enumerate(results):
     put_fact(fact, f"fact{str(index)}")
@@ -53,7 +58,7 @@ for index, fact in enumerate(results):
 
 
 def lambda_handler(event, context):
-    hash_primary = random.randint(0, 9)
+    hash_primary = random.randint(0, FACT_COUNTS)
     functions = [read_fact, redis_cache.get]
     fact = random.choice(functions)(f"fact{str(hash_primary)}")
 
